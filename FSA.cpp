d0a9b0aef219e24a0dc5379c5c518b72992b1da3
Danny Regan <d.regan@columbia.edu>
2025-04-06 19:25:02 +0200

    FSA checks if a string contains 3 letters of the alphabet {a,b,c} and has a triplet (aaa, bbb, etc.).

---
+++ FSA.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class FSA {
+private:
+    enum State { Start, a1, a2, b1, b2, T, F };
+public:
+    State currentState = Start;
+
+    bool WordLength(string input) {
+        if (input.length() < 3) currentState = F;
+        else currentState = T;
+    }
+
+    bool ProcessChar(char ch) {
+        if (ch != 'a' && ch != 'b' && ch != 'c') {
+            currentState = F;
+        }
+
+        switch (currentState) {
+            case Start:
+                if      (ch == 'a') currentState = a1;
+                else if (ch == 'b') currentState = b1;
+                else if (ch == 'c') currentState = Start;
+                else                currentState = F;
+                break;
+            case a1:
+                if      (ch == 'a') currentState = a2;
+                else if (ch == 'b') currentState = b1;
+                else if (ch == 'c') currentState = Start;
+                else                currentState = F;
+                break;
+            case a2:
+                if      (ch == 'a') currentState = T;
+                else if (ch == 'b') currentState = b1;
+                else if (ch == 'c') currentState = Start;
+                else                currentState = F;
+                break;
+            case b1:
+                if      (ch == 'a') currentState = a1;
+                else if (ch == 'b') currentState = b2;
+                else if (ch == 'c') currentState = Start;
+                else                currentState = F;
+                break;
+            case b2:
+                if      (ch == 'a') currentState = a1;
+                else if (ch == 'b') currentState = T;
+                else if (ch == 'c') currentState = Start;
+                else                currentState = F;
+                break;
+        }
+        return (currentState == T) || (currentState == F);
+    }
+
+    bool IsAccepted() const {
+        return currentState == T;
+    }
+};
+
+int main() {
+    FSA fsa;
+    string input;
+    
+    cout << "Enter a string (alphabet {a, b, c}): ";
+    cin >> input;
+
+    fsa.WordLength(input);
+
+    for (char c: input) {
+        fsa.ProcessChar(c);
+    }
+
+    fsa.IsAccepted() ? cout << "String is accepted." << endl : cout << "String is rejected." << endl;
+
+    return 0;
+}
